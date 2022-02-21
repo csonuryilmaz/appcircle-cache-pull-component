8ee6f3d2c864abd1927e3e43de5383e12280e0c1
Onur Yılmaz <csonuryilmaz@gmail.com>
2022-02-21 22:30:24 +0300

    Change nonzero exit conditions with exit=0 and @@[error]

--- main.rb
+++ main.rb
@@ -9,26 +9,34 @@ def get_env_variable(key)
 end
 
 def run_command(command)
-  puts "@@[command] #{command}"
-  exit $CHILD_STATUS.exitstatus unless system(command)
+  unless system(command)
+    puts "Unexpected exit with #{exit $CHILD_STATUS.exitstatus} code. Check logs for details."
+    exit 0
+  end
 end
 
-def run_command_silent(command)
-  exit $CHILD_STATUS.exitstatus unless system(command)
+def run_command_with_log(command)
+  puts "@@[command] #{command}"
+  run_command(command)
+end
+
+def abort_with0(message)
+  puts "@@[error] #{message}"
+  exit 0
 end
 
 ac_repository_path = get_env_variable('AC_REPOSITORY_DIR')
-ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort('Cache label path must be defined.')
+ac_cache_label = get_env_variable('AC_CACHE_LABEL') || abort_with0('Cache label path must be defined.')
 
-ac_token_id = get_env_variable('AC_TOKEN_ID') || abort('AC_TOKEN_ID env variable must be set when build started.')
+ac_token_id = get_env_variable('AC_TOKEN_ID') || abort_with0('AC_TOKEN_ID env variable must be set when build started.')
 ac_callback_url = get_env_variable('ASPNETCORE_CALLBACK_URL') ||
-                  abort('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
+                  abort_with0('ASPNETCORE_CALLBACK_URL env variable must be set when build started.')
 
 signed_url_api = "#{ac_callback_url}?action=getCacheUrls"
 
 # check dependencies
-run_command_silent('unzip -v |head -1')
-run_command_silent('curl --version |head -1')
+run_command('unzip -v |head -1')
+run_command('curl --version |head -1')
 
 @cache = "ac_cache/#{ac_cache_label}"
 zipped = "ac_cache/#{ac_cache_label.gsub('/', '_')}.zip"
@@ -55,23 +63,23 @@ unless ac_token_id.empty?
     puts signed['getUrl']
 
     ENV['AC_CACHE_GET_URL'] = signed['getUrl']
-    run_command_silent("curl -X GET -H \"Content-Type: application/zip\" -o #{zipped} $AC_CACHE_GET_URL")
+    run_command("curl -X GET -H \"Content-Type: application/zip\" -o #{zipped} $AC_CACHE_GET_URL")
   end
 end
 
 exit 0 unless File.size?(zipped)
 exit 0 if system("head -1 #{zipped} |grep -i -q NoSuchKey && rm -f #{zipped}")
 
-run_command("unzip -qq #{zipped}")
+run_command_with_log("unzip -qq #{zipped}")
 
 Dir.glob("#{@cache}/*.zip", File::FNM_DOTMATCH).each do |zip_file|
-  run_command("unzip -qq #{zip_file} -d /")
+  run_command_with_log("unzip -qq #{zip_file} -d /")
 end
 
 Dir.glob("#{@cache}/repository/*.zip", File::FNM_DOTMATCH).each do |zip_file|
   if ac_repository_path
-    run_command("unzip -qq -u #{zip_file} -d #{ac_repository_path}")
+    run_command_with_log("unzip -qq -u #{zip_file} -d #{ac_repository_path}")
   else
-    puts "Warning: #{zip_file} is ignored. It can be used only after Git Clone workflow step."
+    puts "Warning: #{zip_file} is skipped. It can be used only after Git Clone workflow step."
   end
 end
